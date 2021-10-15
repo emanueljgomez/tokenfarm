b593f32b5a466682c635f108fc7a4fcb81eed03c
Emanuel Gomez <emanueljgomez@gmail.com>
2021-10-15 16:40:46 -0300

    Basic TokenFarm.sol logic added. Modified 2_deploy_cotracts.js

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,23 @@
 pragma solidity ^0.5.0;
 
+// Tokens are imported so the contract is aware of them
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
+// 'Token Farm' is a simple staking dapp where
+// the user deposits 'Dai' tokens and receives 'Dapp' tokens
 contract TokenFarm {
     // State variable, this is going to be stored in the blockchain
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    // Constructor function will run only once
+    // whenever the smart contract is deployed to the network
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        // Local variables are asigned to state variables so
+        // they could be accessed by other functions
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
